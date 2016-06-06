6f062307a734ab3b220a8a81e5080347e0ba894c
adesokanayo <adesokanayo@gmail.com>
2016-06-06 13:46:50 +0100

    First Commit

---
+++ ex1/main.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+
+
+func main() {
+    
+    
+var OS int
+var Ayo string
+var learner bool
+
+
+
+year := 2016
+month := "June"
+today := true
+
+
+    
+fmt.Println(OS)
+fmt.Println(Ayo)
+fmt.Println(learner)
+
+
+newvar:= float32(3.14)
+
+
+fmt.Printf( "This month is  %s in the year %d and it is %t\n" , month, year, today )
+fmt.Printf("New Variable is %v", newvar)
+
+}
