3aa23748335ae0a1482775f4273c5745464017eb
adesokanayo <adesokanayo@gmail.com>
2016-07-01 10:23:12 +0100

    interfaces

--- 10 Interfaces/main.go
+++ 10 Interfaces/main.go
@@ -1,15 +1,40 @@
 package main
 
+import "fmt"
+
 type speaker interface {
 	speak()
 }
 
-type english struct {
+type english struct{}
+
+type chinese struct{}
+
+func (english) speak() {
+	fmt.Println("Hello World")
+
 }
 
-type chinese struct {
+func (chinese) speak() {
+	fmt.Println("你好世界")
 }
 
 func main() {
 
+	var sp speaker
+	var e english
+	sp = e
+	sp.speak()
+
+	var c chinese
+	sp = c
+	sp.speak()
+
+	greet(new(english))
+	greet(new(chinese))
+
+}
+
+func greet(sp speaker) {
+	sp.speak()
 }
