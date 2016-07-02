2b2f8e5d395dc9cb49bd53d7786dacabce9df32e
adesokanayo <adesokanayo@gmail.com>
2016-07-02 23:38:40 +0100

    removing spaces

--- 03pointers/main.go
+++ 03pointers/main.go
@@ -1,14 +1,16 @@
 package main
 
 import "fmt"
+import "github.com\adesokanayo\goclass\02 struct"
 
 func main() {
        
           pay:=10
     
     fmt.Printf("Values of int:%d ,Address of int:%d\n" ,pay, &pay)
+    fmt.Printf("Now and Then %s" ,post.email)
     
-    
+
    
     p:= &pay
     
