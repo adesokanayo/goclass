e38b512395fef05fc7c25ac919ae8fcfd29cee49
adesokanayo <adesokanayo@gmail.com>
2016-07-03 21:29:08 +0100

    correction

--- toy/toy.go
+++ toy/toy.go
@@ -2,14 +2,41 @@ package toy
 
 // Toy is exported type
 type Toy struct {
-	Name string
-
-	Weight int
-	onhand string
-	sold   bool
+   Name string
+   Weight int
+	onhand int
+	sold   int
 }
 
-func new(n Name, w Weight) (t Toy) {
+func New(name string, weight int) (*Toy) {
+	return &Toy{
+        Name: name,
+        Weight: weight,
+    }
+}
 
-	return t
+
+
+func (t *Toy) OnHand() int {
+	return t.onHand
+}
+//copied from answers , not too clear, so I had to read 
+// UpdateOnHand updates the on hand count and
+// returns the current value.
+func (t *Toy) UpdateOnHand(count int) int {
+	t.onHand += count
+	return t.onHand
+}
+
+// Sold returns the current number of this
+// toy sold.
+func (t *Toy) Sold() int {
+	return t.sold
+}
+
+// UpdateSold updates the sold count and
+// returns the current value.
+func (t *Toy) UpdateSold(count int) int {
+	t.sold += count
+	return t.sold
 }
