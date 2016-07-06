3009a6727bef2ce69073bd87a5bd0f7e4f7bfb17
adesokanayo <adesokanayo@gmail.com>
2016-07-06 23:02:29 +0100

    spaaces removed

--- toy/toy.go
+++ toy/toy.go
@@ -16,28 +16,23 @@ func New(name string, weight int) *Toy {
 	}
 }
 
-//OnHand is exporetd
+//OnHand is exported
 func (t *Toy) OnHand() int {
 	return t.onHand
 }
 
-//copied from answers , not too clear, so I had to read
-
-// UpdateOnHand updates the on hand count and
-// returns the current value.
+//UpdateOnHand is exported
 func (t *Toy) UpdateOnHand(count int) int {
 	t.onHand += count
 	return t.onHand
 }
 
-// Sold returns the current number of this
-// toy sold.
+// Sold returns the current number of thetoy sold.
 func (t *Toy) Sold() int {
 	return t.sold
 }
 
-// UpdateSold updates the sold count and
-// returns the current value.
+// UpdateSold updates the sold count and returns the current value.
 func (t *Toy) UpdateSold(count int) int {
 	t.sold += count
 	return t.sold
