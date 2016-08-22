08de8aaa86f831f3466fc512e50801416cd1a4d2
adesokanayo <adesokanayo@gmail.com>
2016-08-22 10:47:54 +0100

    updated

--- webapp/db.go
+++ webapp/db.go
@@ -1,4 +1,4 @@
-/*package main
+package main
 
 import (
 	"fmt"
@@ -6,37 +6,17 @@ import (
 
 	"gopkg.in/mgo.v2"
 	"gopkg.in/mgo.v2/bson"
+	"encoding/json"
 )
 
-type Person struct {
-	Name  string
-	Phone string
-}
 
-func main() {
+
+func Dbconnect() {
 	session, err := mgo.Dial("localhost")
 	if err != nil {
 		panic(err)
 	}
-
-	fmt.Println("DB session Established")
 	defer session.Close()
-
-	// Optional. Switch the session to a monotonic behavior.
-	session.SetMode(mgo.Monotonic, true)
-
-	c := session.DB("test").C("people")
-	err = c.Insert(&Person{"Ale", "+55 53 8116 9639"},
-		&Person{"Cla", "+55 53 8402 8510"})
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	result := Person{}
-	err = c.Find(bson.M{"name": "Ale"}).One(&result)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	fmt.Println("Phone:", result.Phone)
 }
+
+/
--- webapp/main.go
+++ webapp/main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"strings"
+
+	"gopkg.in/mgo.v2"
 )
 
 type myHandler struct {
@@ -41,9 +44,51 @@ func (now *myHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	http.Handle("/", new(myHandler))
-	http.ListenAndServe(":4040", nil)
-}
-/*
 
-*/
+	http.Handle("/", new(myHandler))
+	http.Handle("/api/getuser/:uid", new(getuser))
+	http.Handle("/login", new(myHandler))
+	http.ListenAndServe(":4040", nil)
+
+	session, err := mgo.Dial("localhost")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("DB session Established")
+	defer session.Close()
+	session.SetMode(mgo.Monotonic, true)
+	//collection sample record.
+	/*
+
+		session.DB(*mgo).C(Users).Insert(u)
+
+		u:=&Users{
+			username:  "adesokanayo",
+			firstname: "Odunayo",
+			lastname:  "Adesokan",
+			Status:    1,
+			Age:       "40",
+		})
+	*/
+
+}
+
+//Users struct exported
+type Users struct {
+	User      string `json:"username"`
+	FirstName string `json:"firstname"`
+	LastName  string `json:"lastname"`
+	Age       string `json:"age"`
+	Status    int    `json:"status"`
+}
+
+type getuser struct {
+}
+
+func (g *getuser) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	params := r.URL.Query()
+	log.Println(params)
+	uid := params.Get(":uid")
+	fmt.Fprintf(w, "you are get user %s", uid)
+
+}
